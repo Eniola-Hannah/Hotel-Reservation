be77c7d210d55dcf35eb62f136833a544a7d8cd9
Eniola Hannah <heniolahannah@gmail.com>
2025-02-04 13:59:05 +0100

    Adds a query that counts the total number of reservations in the dataset

--- Query.sql
+++ Query.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE Hotel_Reservation;
 USE Hotel_Reservation;
-SELECT * FROM `hotel reservation dataset`;
 
 RENAME TABLE `hotel reservation dataset` TO hotel_reservation_dataset;
+
+SELECT * FROM hotel_reservation_dataset;
+
+-- 1. What is the total number of reservations in the dataset?  
+SELECT COUNT(Booking_ID) FROM hotel_reservation_dataset;
