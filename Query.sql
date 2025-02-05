5a9fe2fac6ede3b594ee850ea59364dc7e6b4bfc
Eniola Hannah <heniolahannah@gmail.com>
2025-02-05 09:41:14 +0100

    Reviews some mistakes

--- Query.sql
+++ Query.sql
@@ -9,22 +9,17 @@ SELECT * FROM hotel_reservation_dataset;
 SELECT COUNT(Booking_ID) AS total_reservations FROM hotel_reservation_dataset;
 
 -- 2. Which meal plan is the most popular among guests?  
-SELECT MAX(type_of_meal_plan) FROM hotel_reservation_dataset;     -- 'Not selected' has the highest count.
-
-SELECT MAX(type_of_meal_plan) FROM hotel_reservation_dataset
-WHERE type_of_meal_plan IN ("Meal Plan 1", "Meal Plan 2");
+SELECT type_of_meal_plan, COUNT(type_of_meal_plan) AS most_popular_meal_plan FROM hotel_reservation_dataset     -- 'Not selected' has the highest count.
+WHERE booking_status = "Not_Canceled"
+GROUP BY type_of_meal_plan
+ORDER BY most_popular_meal_plan DESC LIMIT 1;
 
 -- 3. What is the average price per room for reservations involving children?  
 SELECT avg_price_per_room FROM hotel_reservation_dataset
 WHERE no_of_children != 0;
 
--- SELECT no_of_children, SUM(avg_price_per_room) FROM hotel_reservation_dataset
--- WHERE no_of_children != 0
--- GROUP BY no_of_children;
-
-
 -- 4. How many reservations were made for the year 20XX (replace XX with the desired year)?  
-SET SQL_SAFE_UPDATES =0;
+SET SQL_SAFE_UPDATES = 0;
 
 UPDATE hotel_reservation_dataset
 SET arrival_date = STR_TO_DATE(arrival_date, "%d-%m-%YYYY");
