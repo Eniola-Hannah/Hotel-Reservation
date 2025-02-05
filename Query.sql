f1cb2ff57b607f853be718fac7d4f7ea165891f2
Eniola Hannah <heniolahannah@gmail.com>
2025-02-05 09:14:26 +0100

    Writes a query that checks for the average number of nights (both weekday and weekend) spent by guests for each room types

--- Query.sql
+++ Query.sql
@@ -80,5 +80,8 @@ GROUP BY month_of_the_year
 -- ORDER BY month_of_the_year
 ;
 
+-- 13. What is the average number of nights (both weekend and weekday) spent by guests for each room type?  
+SELECT room_type_reserved,  AVG(no_of_week_nights) + AVG(no_of_weekend_nights) AS avg_number_of_nights FROM hotel_reservation_dataset
+GROUP BY room_type_reserved;
 
 SELECT * FROM hotel_reservation_dataset;
